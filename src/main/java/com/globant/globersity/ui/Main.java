949ea7907d8f2b0d45ed28c76498b91e66788047
Alejandro Fonseca <140533191+alejandroFonsecaGlobant@users.noreply.github.com>
2023-08-24 11:34:02 -0500

    Student class and instances added (#4)

--- src/main/java/com/globant/globersity/ui/Main.java
+++ src/main/java/com/globant/globersity/ui/Main.java
@@ -2,17 +2,24 @@ package main.java.com.globant.globersity.ui;
 
 import main.java.com.globant.globersity.model.FullTimeTeacher;
 import main.java.com.globant.globersity.model.PartTimeTeacher;
+import main.java.com.globant.globersity.model.Student;
 import main.java.com.globant.globersity.utils.Display;
 
 public class Main {
     public static void main(String[] args) {
+        init();
+    }
+
+    public static void init () {
         FullTimeTeacher full1 = new FullTimeTeacher("Obi-Wan Kenobi",1000000,2);
         FullTimeTeacher full2 = new FullTimeTeacher("Lord Sidius",1750000,4);
         PartTimeTeacher part1 = new PartTimeTeacher("Darth Vader", 1000000, 5);
         PartTimeTeacher part2 = new PartTimeTeacher("Han Solo", 2000000, 8);
-        System.out.println(full1.toString() + "\n" + Display.TEXT_DIVISOR);
-        System.out.println(full2.toString() + "\n" + Display.TEXT_DIVISOR);
-        System.out.println(part1.toString() + "\n" + Display.TEXT_DIVISOR);
-        System.out.println(part2.toString());
+        Student student1 = new Student("Miles Morales",18);
+        Student student2 = new Student("Pepito Perez",19);
+        Student student3 = new Student("Mary Jane",21);
+        Student student4 = new Student("John Wick",18);
+        Student student5 = new Student("Mike Wazowski",25);
+        Student student6 = new Student("Giovanni Giorgio",23);
     }
 }
