1aceded4221b06721e1ac8b5a88b37d73d7d7f7b
Alejandro Fonseca Forero <alejandro.fonseca@globant.com>
2023-08-24 11:29:40 -0500

    Student class and instances added

---
+++ src/main/java/com/globant/globersity/model/Student.java
@@ -0,0 +1,32 @@
+package main.java.com.globant.globersity.model;
+
+public class Student {
+    public static int idCounter = 0;
+    public final int id;
+    public String name;
+    public int age;
+
+    public Student (String name, int age) {
+        idCounter += 1;
+        id = idCounter;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + "\nname: " + name + "\nage: " + age;
+    }
+}
